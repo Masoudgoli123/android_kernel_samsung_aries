f64790a6bd84c65e08dd7a47a2b86bc7327147fd
Pranav Vashi <neobuddy89@gmail.com>
2013-10-18 14:38:06 -0700

    cpufreq: Add OndemandX Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,6 +388,9 @@ extern struct cpufreq_governor cpufreq_gov_abyssplug;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandx)
 #endif
 
 /*********************************************************************
